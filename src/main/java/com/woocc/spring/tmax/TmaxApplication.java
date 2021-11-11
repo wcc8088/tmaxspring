0804f226b07406cb830c55a695e959f1c2f7e2fa
cc <wcc8088@naver.com>
2021-11-11 16:22:25 +0900

    first commit

---
+++ src/main/java/com/woocc/spring/tmax/TmaxApplication.java
@@ -0,0 +1,14 @@
+package com.woocc.spring.tmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmaxApplication.class, args);
+		System.out.println("Tmax Springboot Example");
+	}
+
+}
